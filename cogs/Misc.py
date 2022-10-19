1dc0ccf10ea261df80771a4501f5dc005db84679
Meolsei <Meolsei@VS-HOME>
2022-10-18 22:18:45 -0400

    Updated the age command.

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -6,8 +6,8 @@ class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases = ['age'])
-    async def nsfw(self, ctx, birth):
+    @commands.command()
+    async def age(self, ctx, birth):
         """Gives the user the 18+ role if applicable. Can remove the role if the user already has it.
         The format is YYYY-mm-dd. Example: 2000-12-23"""
 
@@ -20,19 +20,21 @@ class Misc(commands.Cog):
         if age <18:
             await ctx.send(f"Role cannot be given, your age is {age}.")
         else:
+                await ctx.message.author.add_roles(role)
+                await ctx.send(f"Role given!")
+
+    @age.error
+    async def age_handle(self, ctx, error):
+        role = ctx.guild.get_role(807955779113451522)
+
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send("Invalid format provided. `YYYY-mm-dd`, or `2004-02-24` for example.")
+        elif isinstance(error, commands.MissingRequiredArgument):
             if role in ctx.message.author.roles:
                 await ctx.message.author.remove_roles(role)
                 await ctx.send("Role removed!")
             else:
-                await ctx.message.author.add_roles(role)
-                await ctx.send(f"Role given!")
-
-    @nsfw.error
-    async def age_handle(self, ctx, error):
-        if isinstance(error, commands.CommandInvokeError):
-            await ctx.send("Invalid format provided. `YYYY-mm-dd`, or `2004-02-24` for example.")
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("No date provided.")
+                await ctx.send("No date provided. `YYYY-mm-dd`, or `2004-02-24` for example.")
         else:
             raise error
 
